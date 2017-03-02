ebb7e996dc2bed702bf335674562bbad5efd18bd
Edney Chantal <praticasolucoesti@gmail.com>
2017-03-01 23:47:31 -0300

    accreceivable

--- src/app/accreceivable/accreceivable.module.ts
+++ src/app/accreceivable/accreceivable.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AccreceivableComponent} from './accreceivable.component' ;
 import {AccReceivableService} from '../dao/accreceivable.dao.service';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   declarations: [AccreceivableComponent],
   providers:[AccReceivableService]
